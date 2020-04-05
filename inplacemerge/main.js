838f94a4974367f281e03263c005d9649c83e4c9
SebastianMestre <mestresebastianunr@gmail.com>
2020-04-05 16:39:39 -0300

    Start moving common algorithms to a single place

--- inplacemerge/main.js
+++ inplacemerge/main.js
@@ -43,34 +43,6 @@ function insert_sort (arr, from, to) {
 	} 
 } 
 
-function gcd (m, n) { 
-	while (n!=0) { let t = m % n; m=n; n=t; } 
-	return m; 
-} 
-
-function reverse (arr, from, to) { 
-	while (from < to) { 
-		exchange(arr, from++, to--); 
-	} 
-} 
-
-function rotate (arr, from, mid, to) { 
-	if (from==mid || mid==to) return; 
-	let n = gcd(to - from, mid - from); 
-	while (n-- != 0) { 
-		let val = item(arr, from+n); 
-		let shift = mid - from; 
-		let p1 = from+n, p2=from+n+shift; 
-		while (p2 != from + n) { 
-			assign(arr, p1, item(arr, p2)); 
-			p1=p2; 
-			if ( to - p2 > shift) p2 += shift; 
-			else p2=from + (shift - (to - p2)); 
-		} 
-		assign(arr, p1, val); 
-	} 
-}
-
 function merge(arr, from, pivot, to, len1, len2) { 
 	if (len1 == 0 || len2==0) return; 
 	if (len1+len2 == 2) { 
