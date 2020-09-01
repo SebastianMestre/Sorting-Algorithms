5f15931ae06bce5a83e691d9d9121bd05e6be188
SebastianMestre <mestresebastianunr@gmail.com>
2020-09-01 15:02:01 -0300

    Move deterministic_select to lib/algorithm.js

--- median/main.js
+++ median/main.js
@@ -1,34 +1,6 @@
 const step = 1;
 const delay = 0;
 
-function deterministic_select (arr, begin, end, k) {
-	while (1){
-		if(end-begin <= 1) return;
-		let n = end - begin;
-		let groupCount = (n/5) | 0;
-
-		let i = 0;
-		for(let j = 0; j+5 <= n; j += 5, i += 1){
-			insertion_sort(arr, begin+j, begin+j+5);
-			exchange(arr, begin+i, begin+j+2);
-		}
-
-		deterministic_select(arr, begin, begin+i, i>>1);
-		exchange(arr, begin, begin+(i>>1));
-		let cut = partition(
-			arr, begin+1, end, it => compare(arr, it, begin) <= 0);
-		exchange(arr, begin, cut-1);
-
-		const pivot_idx = cut-1-begin;
-		if (pivot_idx < k) {
-			k -= cut-begin;
-			begin = cut;
-		} else {
-			end = cut;
-		}
-	}
-}
-
 function main() {
 	const n = 5**3;
 
